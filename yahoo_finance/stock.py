3a6df8478f22d359a429fec0eb341217b2eb3990
Chirag Matkar <chirag.matkar@gmail.com>
2015-05-08 19:35:21 +0530

    Update stock.py

--- yahoo_finance/stock.py
+++ yahoo_finance/stock.py
@@ -1,11 +1,8 @@
 #Script to Download Hitorical finacial data from Yahoo finance in a csv file
-import urllib
-
-base_url = "http://ichart.finance.yahoo.com/table.csv?s="
 def make_url(ticker_symbol):
     return base_url + ticker_symbol
 
-output_path = "C:/path/to/output/directory"
+output_path = "C://Users/Chirag/Dropbox/Programming/chiragworkspace/web_scrapers"
 def make_filename(ticker_symbol, directory="S&P"):
     return output_path + "/" + directory + "/" + ticker_symbol + ".csv"
 
@@ -16,3 +13,11 @@ def pull_historical_data(ticker_symbol, directory="S&P"):
         outfile = open(make_filename(ticker_symbol, directory), "w")
         outfile.write(e.content)
         outfile.close()
+		
+		
+if __name__ == '__main__':
+			ticker=raw_input("Enter Ticker to pull out Data\n")
+			make_url(ticker)
+			make_filename(ticker)
+			pull_historical_data(ticker)
+			
